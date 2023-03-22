cb1b74926b0964c42447a8deb8d95d76bdf190b9
nithin3119@gmail.com <nithin3119@gmail.com>
2023-03-22 17:58:57 +0530

    blog2

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,37 @@
 from django.db import models
 
 # Create your models here.
-
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self) -> str:
+        return self.title
 
 class Post(models.Model):
+    ACTIVE = 'active'
+    DRAFT = 'draft'
+
+    CHOICES_STATUS = (
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    )
+
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
     body = models.TextField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10,choices=CHOICES_STATUS,default=ACTIVE)
+    image = models.ImageField(upload_to='uploads/',blank=True,null=True)
+    
+    
     
     def __str__(self) -> str:
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render,get_list_or_404,redirect
+from django.shortcuts import get_object_or_404, render,get_list_or_404,redirect
 
-from .models import Post
+from django.db.models import Q
+from .models import Post,Category
 from .forms import CommentForm
+
 
 # Create your views here.
 
 
-def details(request,slug):
+def details(request, category_slug, slug):
     # post = get_list_or_404(Post,slug=slug)
     post = Post.objects.get(slug=slug)
     # comment
@@ -19,7 +21,24 @@ def details(request,slug):
             comment.post = post
             comment.save()
 
-            return redirect('post_detail', slug=slug)
+            return redirect('post_detail',category_slug = category_slug, slug=slug)
     else:
         form = CommentForm()
     return render(request, 'blog/details.html',{'post':post, 'form':form})
+
+def category(request, slug):
+    # category = get_object_or_404(Category, slug=slug)
+    category = Category.objects.get(slug=slug)
+    posts = category.posts.filter(status=Post.ACTIVE)
+
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+
+
+
+def search(request):
+    query = request.GET.get('query', '')
+
+    posts = Post.objects.filter(status=Post.ACTIVE).filter(Q(title__icontains=query) | Q(intro__icontains=query) | Q(body__icontains=query))
+
+    return render(request, 'blog/search.html', {'posts': posts, 'query': query})
